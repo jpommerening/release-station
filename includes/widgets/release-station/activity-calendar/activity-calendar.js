98bff2e7dfe78a90d33616bfdf7cd486f334b117
Jonas Pommerening <jonas.pommerening@gmail.com>
2015-03-17 13:23:48 +0100

    Prototyping activity calendar transitions

--- includes/widgets/release-station/activity-calendar/activity-calendar.js
+++ includes/widgets/release-station/activity-calendar/activity-calendar.js
@@ -24,36 +24,148 @@ define( [
       var startOfCalendar = moment(startOfMonth).weekday(-7);
       var endOfCalendar = moment(endOfMonth).weekday(7);
 
-      $scope.month = startOfMonth;
-      $scope.weeks = [];
+      var weeks = generateCalendar( startOfCalendar, endOfCalendar );
 
-      function getRandomForDate(date, amount) {
-         return ( date.isSame( today ) || date.isBefore( today ) ) ? Math.floor(Math.random()*amount) : 0;
+      var targets = {
+         previousMonth: previousMonth,
+         nextMonth: nextMonth,
+         thisMonth: thisMonth
+      };
+
+      updateMetaData( weeks, today, startOfMonth, endOfMonth, targets );
+      updateEventData( weeks, today );
+
+      $scope.weeks = weeks;
+
+      function previousMonth( date ) {
+         var startOfMonth = moment( date ).startOf( 'month' );
+         var endOfMonth = moment( date ).endOf( 'month' );
+         var startOfCalendar = moment(startOfMonth).weekday(-7);
+         var endOfCalendar = moment($scope.weeks[0][0].date);
+
+         var weeks = generateCalendar( startOfCalendar, endOfCalendar );
+
+         updateEventData( weeks, today );
+
+         $scope.weeks.unshift.apply( $scope.weeks, weeks );
+         $scope.prev = true;
+
+         /* this animation specific stuff should go into a directive */
+         setTimeout( function() {
+            $scope.$apply( function() {
+               $scope.active = true;
+               updateMetaData( $scope.weeks, today, startOfMonth, endOfMonth, targets );
+            } );
+         }, 10 );
+
+         setTimeout( function() {
+            $scope.$apply( function() {
+               $scope.prev = false;
+               $scope.active = false;
+               $scope.weeks.splice(6);
+            } );
+         }, 700 );
       }
 
-      var week = [];
-      var date = moment(startOfCalendar);
-      while( date.isBefore(endOfCalendar) ) {
-         week.push( {
-            date: moment(date),
-            isWeekend: date.day() % 6 === 0,
-            isInMonth: date.isSame( startOfMonth ) || ( date.isAfter( startOfMonth ) && date.isBefore( endOfMonth ) ),
-            isPast: date.isBefore( today ),
-            isToday: date.isSame( today ),
-            isFuture: date.isAfter( today ),
-            commits: getRandomForDate( date, 4.1 ),
-            issues: getRandomForDate( date, 2.5 ),
-            releases: getRandomForDate( date, 1.1 )
-         } );
-         date.add(1, 'day');
-         if( week.length === 7 ) {
-            $scope.weeks.push( week );
-            week = [];
-         }
+      function nextMonth( date ) {
+         var startOfMonth = moment( date ).startOf( 'month' );
+         var endOfMonth = moment( date ).endOf( 'month' );
+         var startOfCalendar = moment($scope.weeks[$scope.weeks.length-1][6].date).add(1, 'day');
+         var endOfCalendar = moment(endOfMonth).weekday(7);
+
+         var weeks = generateCalendar( startOfCalendar, endOfCalendar );
+
+         updateEventData( weeks, today );
+
+         $scope.weeks.push.apply( $scope.weeks, weeks );
+         $scope.next = true;
+
+         /* this animation specific stuff should go into a directive */
+         setTimeout( function() {
+            $scope.$apply( function() {
+               $scope.active= true;
+               updateMetaData( $scope.weeks, today, startOfMonth, endOfMonth, targets );
+            } );
+         }, 10 );
+
+         setTimeout( function() {
+            $scope.$apply( function() {
+               $scope.next = false;
+               $scope.active = false;
+               $scope.weeks.splice(0, 4);
+            } );
+         }, 700 );
+      }
+
+      function thisMonth( date ) {
       }
    }
 
    module.controller( 'ActivityCalendarController', Controller );
+
+   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+   function getWeek( date ) {
+      var startOfWeek = moment( date ).startOf( 'day' ).weekday( -7 );
+
+      return new Array( 7 ).map( function( value, index ) {
+         return { date: moment( startOfWeek ).days( index ) };
+      } );
+   }
+
+   function generateCalendar( startOfCalendar, endOfCalendar ) {
+      var date = moment( startOfCalendar );
+      var week = [];
+      var weeks = [];
+      while( date.isBefore( endOfCalendar ) ) {
+         week.push( {
+            date: moment( date ),
+            weekend: date.day() % 6 === 0
+         } );
+
+         date.add(1, 'day');
+         if( week.length === 7 ) {
+            weeks.push( week );
+            week = [];
+         }
+      }
+      return weeks;
+   }
+
+   function updateMetaData( weeks, today, startOfMonth, endOfMonth, targets ) {
+      weeks.forEach( function( week ) {
+         week.forEach( function( day ) {
+            var date = day.date;
+            day.isInMonth = date.isSame( startOfMonth ) || ( date.isAfter( startOfMonth ) && date.isBefore( endOfMonth ) );
+            day.isPast = date.isBefore( today );
+            day.isToday = date.isSame( today );
+            day.isFuture = date.isAfter( today );
+
+            if( date.isBefore( startOfMonth ) ) {
+               day.target = targets.previousMonth
+            } else if( date.isAfter( endOfMonth ) ) {
+               day.target = targets.nextMonth;
+            } else {
+               day.target = targets.thisMonth;
+            }
+         } );
+      } );
+   }
+
+   function updateEventData( weeks, today ) {
+      function getRandomForDate( date, amount ) {
+         return ( date.isSame( today ) || date.isBefore( today ) ) ? Math.floor(Math.random()*amount) : 0;
+      }
+
+      weeks.forEach( function( week ) {
+         week.forEach( function( day ) {
+            day.commits  = getRandomForDate( day.date, 4.1 );
+            day.issues   = getRandomForDate( day.date, 2.5 );
+            day.releases = getRandomForDate( day.date, 1.1 );
+         } );
+      } );
+   }
+
 
    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
