6548590e7679a607415b74458faec166825fc901
Jonas Pommerening <jonas.pommerening@gmail.com>
2015-06-08 12:29:40 +0200

    Favicon, updated build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,16 @@
 /**
- * Copyright 2015 LaxarJS
+ * Copyright 2015 aixigo AG
  * Released under the MIT license.
- * laxarjs.github.io/release-station
+ * www.laxarjs.org
  */
 /*global module,__dirname,__filename */
 module.exports = function( grunt ) {
    'use strict';
 
-   var path = require( 'path' );
+   require( 'load-grunt-tasks' )( grunt );
 
    var serverPort = 8000;
+   var testPort = 1000 + serverPort;
    var liveReloadPort = 30000 + serverPort;
 
    grunt.initConfig( {
@@ -18,11 +19,31 @@ module.exports = function( grunt ) {
          options: {
             port: serverPort
          },
-         default: {}
+         default: {},
+         test: {
+            options: {
+               port: testPort,
+               keepalive: false
+            }
+         },
+         keepalive: {
+            options: {
+               port: serverPort,
+               keepalive: true
+            }
+         }
       },
       jshint: {
          options: {
             jshintrc: __dirname + '/.jshintrc'
+         }
+      },
+      karma: {
+         options: {
+            reporters: [ 'progress', 'junit' ],
+            proxies: {
+               '/base': 'http://localhost:' + testPort
+            }
          }
       },
       compress: {
@@ -33,13 +54,12 @@ module.exports = function( grunt ) {
             },
             files: [ {
                src: [
-                  '*.+(css|html|js|json)',
+                  '*.{html,js,json}',
                   'application/**',
                   'bower_components/**',
-                  'includes/+(controls|lib|themes|widgets)/**',
-                  'static/**',
+                  'includes/{controls,lib,themes,widgets}/**',
                   'var/**',
-                  '!includes/**/+(bower_components|node_modules)/**'
+                  '!includes/**/{bower_components,node_modules}/**'
                ],
                filter: 'isFile'
             } ]
@@ -57,59 +77,76 @@ module.exports = function( grunt ) {
             src: [ 'application/flow/*.json' ]
          }
       },
+      cssmin: {
+         default: {
+            options: {
+               keepSpecialComments: 0
+            },
+            files: [ {
+               expand: true,
+               src: 'var/static/css/*.theme.css',
+               ext: '.theme.css'
+            } ]
+         }
+      },
       directory_tree: {
          application: {
             dest: 'var/listing/application_resources.json',
             src: [
-               'application/flow/**/*.json',
-               'application/layouts/**/*.css',
-               'application/layouts/**/*.html',
-               'application/pages/**/*.json'
+               'application/{flow,pages}/**/*.json',
+               'application/layouts/**/*.{css,html}'
             ],
             options: {
                embedContents: [
-                  'application/flow/**/*.json',
-                  'application/layouts/**/*.html',
-                  'application/pages/**/*.json'
+                  'application/{flow,pages}/**/*.json',
+                  'application/layouts/**/*.html'
                ]
             }
          },
          bower_components: {
             dest: 'var/listing/bower_components_resources.json',
             src: [
-               'bower_components/laxar-uikit/dist/themes/**/*.css',
-               'bower_components/laxar-uikit/dist/controls/**/*.+(json|css|html)',
-               'bower_components/laxar-*-control/control.json',
-               'bower_components/laxar-*-control/*.theme/*.+(css|html)',
-               'bower_components/laxar-*-widget/widget.json',
-               'bower_components/laxar-*-widget/*.theme/*.+(css|html)'
+               'bower_components/laxar-uikit/dist/themes/default.theme/css/theme.css',
+               'bower_components/laxar-uikit/dist/controls/*/{control.json,*.theme/css/*.css}',
+               'bower_components/laxar-*-control/{control.json,*.theme/css/*.css}'
             ],
-            embedContents: [ 'bower_components/laxar-uikit/dist/controls/**/*.html' ]
+            options: {
+               embedContents: [
+                  'bower_components/laxar-uikit/dist/controls/*/control.json'
+               ]
+            }
          },
          includes: {
             dest: 'var/listing/includes_resources.json',
             src: [
-               'includes/themes/**/*.+(css|html)',
-               'includes/widgets/*/*/*.+(css|html|json)',
-               '!includes/widgets/*/*/+(package|bower).json',
-               'includes/widgets/*/*/!(bower_components|node_modules|spec)/**/*.+(css|html|json)'
+               'includes/themes/*.theme/{css,controls,widgets,layouts}/**/*.{css,html}',
+               'includes/controls/*/*/{control.json,*.theme/css/*.css}',
+               'includes/widgets/*/*/{widget.json,*.theme/css/*.css,*.theme/*.html}'
             ],
             options: {
                embedContents: [
-                  'includes/themes/**/controls/**/*.html',
-                  'includes/widgets/*/*/widget.json',
-                  'includes/widgets/*/*/*.theme/*.html',
-                  'includes/widgets/*/*/*.theme/css/*.css'
+                  'includes/controls/**/control.json',
+                  'includes/widgets/*/*/{widget.json,*.theme/*.html}'
                ]
             }
+         }
+      },
+      concat: {
+         build: {
+            src: [
+               'require_config.js',
+               'bower_components/requirejs/require.js'
+            ],
+            dest: 'var/build/require_configured.js'
          }
       },
       requirejs: {
          default: {
             options: {
                mainConfigFile: 'require_config.js',
+               deps: [ '../var/build/require_configured' ],
                name: '../init',
-               out: 'var/build/optimized_init.js',
+               out: 'var/build/bundle.js',
                optimize: 'uglify2'
             }
          }
@@ -130,7 +167,8 @@ module.exports = function( grunt ) {
          libraries: {
             files: [
                'includes/lib/*/!(bower_components|node_modules)/**',
-               'includes/themes/*.theme/!(bower_components|node_modules)/**'
+               'includes/themes/*.theme/!(bower_components|node_modules)/**',
+               '!includes/**/test-results.xml'
             ]
          },
          dependencies: {
@@ -150,33 +188,42 @@ module.exports = function( grunt ) {
       }
    } );
 
-   // Find all widget.json files,
-   // take their directory names,
-   // create or update the configuration
+   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+   var path = require( 'path' );
+
    grunt.file.expand( 'includes/widgets/*/*/widget.json' )
       .map( path.dirname )
-      .forEach( function( widget ) {
-         var config = grunt.config( 'widget.' + widget );
-         grunt.config( 'widget.' + widget, grunt.util._.defaults( {}, config ) );
-         grunt.config( 'watch.' + widget, {
-            files: [
-               widget + '/!(bower_components|node_modules)',
-               widget + '/!(bower_components|node_modules)/**'
-            ]
-         } );
+      .forEach( configureArtifact.bind( null, 'widget' ) );
+
+   grunt.file.expand( 'includes/controls/**/control.json' )
+      .map( path.dirname )
+      .forEach( configureArtifact.bind( null, 'control' ) );
+
+   function configureArtifact( type, artifact ) {
+      var config = grunt.config( type + '.' + artifact ) || {};
+      config.karma = config.karma || { junitReporter: { outputFile: artifact + '/test-results.xml' } };
+      grunt.config( type + '.' + artifact, config );
+
+      grunt.config( 'watch.' + type + '_' + artifact, {
+         files: [
+            artifact + '/!(bower_components|node_modules)',
+            artifact + '/!(bower_components|node_modules)/**',
+            '!' + artifact + '/test-results.xml'
+         ]
       } );
+   }
 
-   grunt.loadNpmTasks( 'grunt-laxar' );
-   grunt.loadNpmTasks( 'grunt-contrib-compass' );
-   grunt.loadNpmTasks( 'grunt-contrib-compress' );
-   grunt.loadNpmTasks( 'grunt-contrib-watch' );
+   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-   grunt.registerTask( 'server', [ 'connect' ] );
+   grunt.registerTask( 'server', [ 'connect:default' ] );
    grunt.registerTask( 'build', [ 'directory_tree', 'laxar_application_dependencies' ] );
-   grunt.registerTask( 'optimize', [ 'build', 'css_merger', 'requirejs' ] );
-   grunt.registerTask( 'test', [ 'server', 'widgets' ] );
-   grunt.registerTask( 'default', [ 'build', 'test' ] );
+   grunt.registerTask( 'optimize', [ 'build', 'css_merger', 'cssmin', 'concat', 'requirejs' ] );
+   grunt.registerTask( 'test', [ 'connect:test', 'widgets' ] );
    grunt.registerTask( 'dist', [ 'optimize', 'compress' ] );
    grunt.registerTask( 'start', [ 'build', 'server', 'watch' ] );
+   grunt.registerTask( 'start-no-watch', [ 'build', 'connect:keepalive' ] );
+
+   grunt.registerTask( 'default', [ 'build', 'test' ] );
 
 };
